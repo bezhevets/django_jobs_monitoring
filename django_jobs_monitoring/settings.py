82fff82e5bb5adf02eeb16df6f36ad9ca5050c49
Maksym Bezhevets <begik4@gmail.com>
2024-03-03 01:27:39 +0200

    Create app and model Djinni

--- django_jobs_monitoring/settings.py
+++ django_jobs_monitoring/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "user",
     "job_boards",
+    "djinni",
 ]
 
 MIDDLEWARE = [
---
+++ djinni/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils.timezone import now
+
+
+class VacancyDjinni(models.Model):
+    title = models.CharField(max_length=255)
+    intro = models.CharField(max_length=255)
+    description = models.TextField()
+    company = models.CharField(max_length=100, null=True)
+    location = models.CharField(max_length=190, null=True)
+    employment_type = models.CharField(max_length=190, null=True)
+    experience = models.CharField(max_length=100, null=True)
+    english = models.CharField(max_length=100, null=True)
+    reviews = models.IntegerField()
+    date = models.DateTimeField(default=now)
+
+    class Meta:
+        verbose_name = "Vacancy Djinni"
+        verbose_name_plural = "Vacancies Djinni"
+
+    def __str__(self) -> str:
+        return f"{self.company} is looking for a {self.title} with experience {self.experience}"
