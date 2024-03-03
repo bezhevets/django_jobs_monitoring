86fdfc88b7a208d26562a45fe878146b084363b6
Maksym Bezhevets <begik4@gmail.com>
2024-03-03 19:24:49 +0200

    Changed djinni models

--- djinni/models.py
+++ djinni/models.py
@@ -6,13 +6,14 @@ class VacancyDjinni(models.Model):
     title = models.CharField(max_length=255)
     intro = models.CharField(max_length=255)
     description = models.TextField()
-    company = models.CharField(max_length=100, null=True)
+    company = models.CharField(max_length=255, null=True)
     location = models.CharField(max_length=190, null=True)
     employment_type = models.CharField(max_length=190, null=True)
     experience = models.CharField(max_length=100, null=True)
     english = models.CharField(max_length=100, null=True)
     reviews = models.IntegerField()
-    date = models.DateTimeField(default=now)
+    date = models.DateTimeField()
+    url = models.URLField(unique=True)
 
     class Meta:
         verbose_name = "Vacancy Djinni"
